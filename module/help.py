d9b5c87d4a0dcadd7fb7d4b1c77de3242652a835
Jiwonii97 <wldnjs3633@naver.com>
2022-09-20 16:28:08 +0900

    '!도움' Command function

---
+++ module/help.py
@@ -0,0 +1,18 @@
+from module.utils import printCommandLogs
+
+commandList = {
+    "!출근" : "현재시간으로 출근 처리가 진행됩니다.",
+    "!퇴근" : "현재시간으로 퇴근 처리가 진행됩니다.",
+    "!자리비움 (시간)" : "현재시간부터 해당 시간만큼 자리비움(오프) 상태로 처리됩니다.",
+    "!도움" : "실행가능한 전체 명령어에 대해 나오게 됩니다."
+}
+
+# !도움
+def helpCommend(message, say):
+    helpOutput = "\n< 명령어 도움말 >\n"
+    
+    for command, info in commandList.items():
+        helpOutput += f"{command} : {info}\n"
+    
+    printCommandLogs(message, "!도움")
+    say(text=helpOutput, channel = message['channel'])

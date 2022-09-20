9dd19d7280ae1c7f6a74ea88d564d44c31d4a915
Jiwonii97 <wldnjs3633@naver.com>
2022-09-20 16:27:25 +0900

    SlackBot 내부적으로 실행되는 세부적인 함수

---
+++ module/utils.py
@@ -0,0 +1,30 @@
+# get Time
+from datetime import datetime
+from pytz import timezone
+
+# token
+import json
+
+# Common Variables
+tokenLink = 'assets/slack_token.json'
+READ_MODE = 'r'
+
+
+# functions
+def getCurrentSeoulTime():
+    return datetime.now(timezone('Asia/Seoul'))
+
+def getSlackToken():
+    # go to Keys and get dictionary
+    with open(tokenLink, READ_MODE) as f:
+        keys = json.load(f)
+        
+    # get Token
+    slack_bot_token = keys['slack_token']['slack_bot_token']
+    slack_app_token = keys['slack_token']['slack_app_token']
+    return slack_app_token, slack_bot_token
+
+def printCommandLogs(message, command):
+    user = message['user']
+    currnetTime = getCurrentSeoulTime().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{currnetTime}\t{user}가 {command} 명령어를 사용하였습니다")

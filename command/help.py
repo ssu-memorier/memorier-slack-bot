11be3d82d5da8ff505f0d91d4e1a5db0d274ac3b
Jiwonii97 <wldnjs3633@naver.com>
2022-09-22 19:52:18 +0900

    일부 추가적인 상수처리 진행

--- command/help.py
+++ command/help.py
@@ -1,20 +1,13 @@
-from util.utils import printCommandLogs
-from constants.constants import *       # 상수
+from util.log import printCommandLogs
+from constants import COMMAND, HELP       # 상수
 
-commandDict = {
-    "!출근" : "현재시간으로 출근 처리가 진행됩니다.",
-    "!퇴근" : "현재시간으로 퇴근 처리가 진행됩니다.",
-    "!오프 or !자리비움 (시간)" : "현재시간부터 해당 시간만큼 자리비움(오프) 상태로 처리됩니다.",
-    "!도움" : "실행가능한 전체 명령어에 대해 나오게 됩니다."
-}
 
-# !도움
-def helpCommand(message, say):
-    helpOutput = HELP_HEADLINE
-    
-    for command, info in commandDict.items():
-        helpOutput += f"{command} : {info}\n"
-    
-    printCommandLogs(message, HELP)      # 서버에 log를 따로 남김
-    say(text=helpOutput, channel = message['channel'])
-    
+def helpCommand(message, say):      # 도움
+    helpOutput = HELP.HELP_HEADLINE
+
+    for command in COMMAND.ATTENDANCE_COMMAND_LIST:
+        helpOutput += f"{COMMAND.ATTENDANCE_COMMAND[command]} : {HELP.COMMAND_HELP_MESSAGE[command]}\n"
+
+    # 서버에 log를 따로 남김
+    printCommandLogs(message, COMMAND.ATTENDANCE_COMMAND[COMMAND.HELP])
+    say(text=helpOutput, channel=message['channel'])

463dff2f32254deca671ab8aebb752f0f02bc764
Jiwonii97 <wldnjs3633@naver.com>
2022-09-22 19:49:20 +0900

    app 함수 모듈별로 코드 정리 진행

--- app.py
+++ app.py
@@ -1,31 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# Module Function
-from util.utils import getCurrentSeoulTime, getSlackToken
-from command.help import helpCommand
-from constants.constants import *       # 상수
+# User Defined Functions
+from command.commands import getAttendanceCommand
+from constants import TOKEN      # Slack App/Bot Token
 
 # Python Libraries
-import re
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# get Token
-slackAppToken, slackBotToken = getSlackToken()
+# App Loading
+app = App(token=TOKEN.SLACK_BOT_TOKEN)
+getAttendanceCommand(app)
 
-# 앱 실행
-app = App(token=slackBotToken)
-    
-# !도움말
-@app.message(re.compile((HELP)))
-def say_comment_help(message, say):
-    helpCommand(message, say)
 
-# 메인 진행 함수
-def main():
-    SocketModeHandler(app, slackAppToken).start()
+def main():  # Main Process
+    SocketModeHandler(app, TOKEN.SLACK_APP_TOKEN).start()
 
-# main 실행문
+
 if __name__ == "__main__":
     main()
-    

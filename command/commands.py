3ec7e0f3cfc0b3d5569bb8dc9ddf343547593192
Jiwonii97 <wldnjs3633@naver.com>
2022-09-22 19:51:22 +0900

    SlackBot의 Command 함수 추가(Attendance Command)

---
+++ command/commands.py
@@ -0,0 +1,11 @@
+import re       # 정규식
+
+from constants.COMMAND import *
+from command import help
+
+
+def getAttendanceCommand(app):
+
+    @app.message(re.compile(ATTENDANCE_COMMAND[HELP]))    # !도움말
+    def say_comment_help(message, say):
+        help.helpCommand(message, say)

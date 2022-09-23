2ea15b8c3dc6c785617a37dfeb81caf5fd6ea89a
Jiwonii97 <wldnjs3633@naver.com>
2022-09-23 19:01:01 +0900

    !출근, !퇴근, !오프 기능 추가로 인한 함수 수정

--- app.py
+++ app.py
@@ -1,37 +1,23 @@
 # -*- coding: utf-8 -*-
-from module.utils import getCurrentSeoulTime, getSlackToken
-from module.attendance import GoToWork, LeaveToWork, OfflineWork
 
-import re
+# User Defined Functions
+from command.commands import addAttendanceCommand
+from constants import TOKEN      # Slack App/Bot Token
+
+# Python Libraries
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# get Token
-slackAppToken, slackBotToken = getSlackToken()
 
-# 앱 실행
-app = App(token=slackBotToken)
-    
-# !출근
-@app.message(re.compile("(!출근)"))
-def say_comment_gotowork(message, say):
-    GoToWork(message, say)
+# App Loading
+app = App(token=TOKEN.SLACK_BOT_TOKEN)
+addAttendanceCommand(app)
 
-# !퇴근
-@app.message(re.compile(("!퇴근")))
-def say_comment_leavetowork(message, say):
-    LeaveToWork(message, say)
 
-# !오프 (시간)
-@app.message(re.compile("(!오프) \d"))
-def say_comment_off(message, say):
-    OfflineWork(message, say)
+def main():  # Main Process
+    print("="*10, "SlackBot is started", "="*10, '\n'*2)       # slackbot 시작
+    SocketModeHandler(app, TOKEN.SLACK_APP_TOKEN).start()
 
-# 메인 진행 함수
-def main():
-    print("="*10,"SlackBot is started","="*10,'\n'*2)       # slackbot 시작
-    SocketModeHandler(app, slackAppToken).start()
 
-# main 실행문
 if __name__ == "__main__":
     main()

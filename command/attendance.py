fb17169872981759406f314d96c4389dc8be2a2d
Jiwonii97 <wldnjs3633@naver.com>
2022-09-25 02:18:12 +0900

    코어타임 명령어 제한 기능 추가

--- command/attendance.py
+++ command/attendance.py
@@ -2,7 +2,7 @@ from constants.COMMAND import *     # 상수
 from constants.DATE import *
 
 from utils.date import getCurrentSeoulTime
-from utils.log import printCommandLogs, printWorkState
+from utils.log import printCommandLogs, printWorkState, printCommandLogsInCoreTime
 
 
 def GoToWork(message, say):  # !출근
@@ -22,16 +22,39 @@ def GoToWork(message, say):  # !출근
 
 
 def LeaveToWork(message, say):  # !퇴근
+    state = LTW
     date = getCurrentSeoulTime()       # 현재시간을 가져온다
-    workOutput = printWorkState(message, date, LTW)
+    hour = date.time().hour         # 명령어를 실행한 시(HH)
 
-    printCommandLogs(message, ATTENDANCE_COMMAND[LTW])      # 로그 출력
-    say(text=workOutput, channel=CHANNEL_ATTENDANCE)
+    # 코어타임 이전에 퇴근을 시도하는 경우
+    if CORETIME[START_TIME][HH] <= hour < CORETIME[END_TIME][HH]:
+        printCommandLogsInCoreTime(message, state)      # 로그에 해당내용 기록
+        workOutput = CORETIME_OUTPUT[state]        # 해당 명령어 입력하는 부분에 경고문 출력
+
+        # message['channel'] : 현재 봇과 대화하는 채널
+        say(text=workOutput, channel=message['channel'])
+
+    else:
+        workOutput = printWorkState(message, date, LTW)
+
+        printCommandLogs(message, ATTENDANCE_COMMAND[LTW])      # 로그 출력
+        say(text=workOutput, channel=CHANNEL_ATTENDANCE)
 
 
 def OfflineWork(message, say):  # !오프 (시간)
+    state = OW
     date = getCurrentSeoulTime()       # 현재시간을 가져온다
-    workOutput = printWorkState(message, date, OW)
+    hour = date.time().hour         # 명령어를 실행한 시(HH)
 
-    printCommandLogs(message, ATTENDANCE_COMMAND[OW])      # 로그 출력
-    say(text=workOutput, channel=CHANNEL_ATTENDANCE)
+    if CORETIME[START_TIME][HH] <= hour < CORETIME[END_TIME][HH]:
+        printCommandLogsInCoreTime(message, state)      # 로그에 해당내용 기록
+        workOutput = CORETIME_OUTPUT[state]        # 해당 명령어 입력하는 부분에 경고문 출력
+
+        # message['channel'] : 현재 봇과 대화하는 채널
+        say(text=workOutput, channel=message['channel'])
+
+    else:
+        workOutput = printWorkState(message, date, OW)
+
+        printCommandLogs(message, ATTENDANCE_COMMAND[OW])      # 로그 출력
+        say(text=workOutput, channel=CHANNEL_ATTENDANCE)

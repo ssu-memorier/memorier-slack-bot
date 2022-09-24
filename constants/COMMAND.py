a8e5b737d74f321f5d13674bb942388cd788ed3d
Jiwonii97 <wldnjs3633@naver.com>
2022-09-25 02:18:21 +0900

    코어타임 관련 상수 추가

--- constants/COMMAND.py
+++ constants/COMMAND.py
@@ -24,3 +24,13 @@ ATTENDANCE_COMMAND = {        # Command Keyword
 # MESSAGE TOKEN에서 값을 나타내는 인덱스
 MESSAGE_TOKEN_COMMAND = 0
 MESSAGE_TOKEN_TIME = 1
+
+# 코어타임 관련 Log, Output TEXT 목록
+CORETIME_LOG = {
+    LTW: "Error log\tCore Time에 !퇴근을 시도하였습니다.",
+    OW: "Error log\tCore Time에 !오프를 시도하였습니다."
+}
+CORETIME_OUTPUT = {
+    LTW: ":warning:\t현재는 Core Time (20:00:00~23:00:00) 으로 퇴근 하실 수 없습니다.",
+    OW: ":warning:\t현재는 Core Time (20:00:00~23:00:00) 으로 오프 하실 수 없습니다."
+}

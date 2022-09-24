6ef7edcf63314abb9c2d780627df7358c44e65fa
Jiwonii97 <wldnjs3633@naver.com>
2022-09-25 01:47:00 +0900

    시간데이터 상수화 진행

--- command/attendance.py
+++ command/attendance.py
@@ -1,4 +1,5 @@
 from constants.COMMAND import *     # 상수
+from constants.DATE import *
 
 from utils.date import getCurrentSeoulTime
 from utils.log import printCommandLogs, printWorkState
@@ -6,9 +7,10 @@ from utils.log import printCommandLogs, printWorkState
 
 def GoToWork(message, say):  # !출근
     dates = getCurrentSeoulTime()       # 현재시간을 가져온다
+    hour, minutes = dates.time().hour, dates.time().minute   # 명령어를 실행한 시(HH), 분(MM)
 
     # 10시 10분 넘어서 출근을 하면 지각 처리
-    if dates.time().hour >= 10 and dates.time().minute >= 10:
+    if hour >= LATE_TIME[HOUR] and minutes >= LATE_TIME[MIN]:
         state = LATE
     else:
         state = GTW

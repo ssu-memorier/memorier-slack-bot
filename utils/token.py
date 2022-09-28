5f0de67f5d1de1b89e71d3f3f343984065e14cdf
Jiwonii97 <wldnjs3633@naver.com>
2022-09-29 00:12:31 +0900

    테스트 토큰 수정

--- utils/token.py
+++ utils/token.py
@@ -6,13 +6,13 @@ def getSlackToken(tokenPath, mode):     # slack Token을 가져오는 함수
     with open(tokenPath, mode, encoding='utf-8') as f:
         keys = json.load(f)
 
-    # # get Token
-    # slack_bot_token = keys['slack_token']['slack_bot_token']
-    # slack_app_token = keys['slack_token']['slack_app_token']
+    # get Token
+    slack_bot_token = keys['slack_token']['slack_bot_token']
+    slack_app_token = keys['slack_token']['slack_app_token']
 
-    # get test Token : test 진행할 때 사용하는 TOKEN (평상시 사용 X)
-    slack_bot_token = keys['slack_test_token']['slack_bot_token']
-    slack_app_token = keys['slack_test_token']['slack_app_token']
+    # # get test Token : test 진행할 때 사용하는 TOKEN (평상시 사용 X)
+    # slack_bot_token = keys['slack_test_token']['slack_bot_token']
+    # slack_app_token = keys['slack_test_token']['slack_app_token']
 
     return slack_app_token, slack_bot_token
 
@@ -23,10 +23,10 @@ def getBotChannel(tokenPath, mode):     # 봇 채널ID를 가져오는 함수
         keys = json.load(f)
 
     # # get Token
-    # slack_bot_channelID = keys['slack_token']['slack_bot_channelID']
+    slack_bot_channelID = keys['slack_token']['slack_bot_channelID']
 
     # get test Token : test 진행할 때 사용하는 TOKEN (평상시 사용 X)
-    slack_bot_channelID = keys['slack_test_token']['slack_bot_channelID']
+    # slack_bot_channelID = keys['slack_test_token']['slack_bot_channelID']
 
     return slack_bot_channelID
 

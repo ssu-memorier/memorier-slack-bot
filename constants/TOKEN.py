2601fdbd5413176d1e6fe9c306e6c6058a5f5979
Jiwonii97 <wldnjs3633@naver.com>
2022-09-28 18:45:10 +0900

    key로부터 user_id를 불러오도록 내용 수정

--- constants/TOKEN.py
+++ constants/TOKEN.py
@@ -1,9 +1,13 @@
 # TOKEN 관련 상수
-from utils.token import getSlackToken
+from utils.token import getSlackToken, getUserId
 
-# Constants
+# Constants to get TOKEN
 TOKEN_LINK = 'assets/slack_token.json'
+USER_LINK = 'assets/user_info.json'
 READ_MODE = 'r'
 
 # token
 SLACK_APP_TOKEN, SLACK_BOT_TOKEN = getSlackToken(TOKEN_LINK, READ_MODE)
+
+# User name in team
+USER_NAME = getUserId(USER_LINK, READ_MODE)
--- utils/token.py
+++ utils/token.py
@@ -6,8 +6,19 @@ def getSlackToken(tokenPath, mode):
     with open(tokenPath, mode) as f:
         keys = json.load(f)
 
-    # get Token
-    slack_bot_token = keys['slack_token']['slack_bot_token']
-    slack_app_token = keys['slack_token']['slack_app_token']
+    # # get Token
+    # slack_bot_token = keys['slack_token']['slack_bot_token']
+    # slack_app_token = keys['slack_token']['slack_app_token']
+
+    # get test Token
+    slack_bot_token = keys['slack_test_token']['slack_bot_token']
+    slack_app_token = keys['slack_test_token']['slack_app_token']
 
     return slack_app_token, slack_bot_token
+
+
+def getUserId(userPath, mode):
+    with open(userPath, mode) as f:
+        keys = json.load(f)
+
+    return keys["user_id"]

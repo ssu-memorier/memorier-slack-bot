eebf5ee4ced74f8ca08433eff850b8cac071705d
Jiwonii97 <wldnjs3633@naver.com>
2022-09-28 18:54:19 +0900

    테스트 토큰 -> slack API 토큰 변경

--- utils/token.py
+++ utils/token.py
@@ -3,22 +3,22 @@ import json
 
 def getSlackToken(tokenPath, mode):
     # go to Keys and get dictionary
-    with open(tokenPath, mode) as f:
+    with open(tokenPath, mode, encoding='utf-8') as f:
         keys = json.load(f)
 
-    # # get Token
-    # slack_bot_token = keys['slack_token']['slack_bot_token']
-    # slack_app_token = keys['slack_token']['slack_app_token']
+    # get Token
+    slack_bot_token = keys['slack_token']['slack_bot_token']
+    slack_app_token = keys['slack_token']['slack_app_token']
 
-    # get test Token
-    slack_bot_token = keys['slack_test_token']['slack_bot_token']
-    slack_app_token = keys['slack_test_token']['slack_app_token']
+    # # get test Token
+    # slack_bot_token = keys['slack_test_token']['slack_bot_token']
+    # slack_app_token = keys['slack_test_token']['slack_app_token']
 
     return slack_app_token, slack_bot_token
 
 
 def getUserId(userPath, mode):
-    with open(userPath, mode) as f:
+    with open(userPath, mode, encoding='utf-8') as f:
         keys = json.load(f)
 
     return keys["user_id"]

e58ffd06a52c9573d1dc70d065675ddc54de5256
Jiwonii97 <wldnjs3633@naver.com>
2022-09-28 18:56:24 +0900

    메시지 객체 클래스 추가 및 일부 호출 코드 수정

--- utils/manageCommands.py
+++ utils/manageCommands.py
@@ -1,7 +1,15 @@
 import re       # 정규식
 
-from constants import COMMAND
+from constants import COMMAND       # 상수
 from command import help, attendance      # help, 출석 관련 명령어
+
+
+class appMessage():     # app으로 부터 받은 메시지 정보를 관리하는 클래스
+    def __init__(self, message):
+        self.channel = message['channel']
+        self.text = message['text']
+        self.ts = message['ts']
+        self.userID = message['user']
 
 
 def addHelpCommand(app):      # 도움말 관련 명령어 추가
@@ -9,22 +17,22 @@ def addHelpCommand(app):      # 도움말 관련 명령어 추가
     # !도움말
     @app.message(re.compile(COMMAND.HELP_REG))
     def sayCommentHelp(message, say):
-        help.helpCommand(message, say)
+        help.helpCommand(appMessage(message), say)
 
 
 def addAttendanceCommand(app):      # 출석 관련 명령어 추가
 
     @app.message(re.compile(COMMAND.GOTOWORK_REG))  # !출근
     def sayCommentGotowork(message, say):
-        attendance.GoToWork(message, say)
+        attendance.GoToWork(appMessage(message), say)
 
     @app.message(re.compile(COMMAND.LEAVETOWORK_REG))  # !퇴근
     def sayCommentLeavetowork(message, say):
-        attendance.LeaveToWork(message, say)
+        attendance.LeaveToWork(appMessage(message), say)
 
     @app.message(re.compile(COMMAND.OFFLINE_REG))  # !오프 (시간)
     def sayCommentOff(message, say):
-        attendance.OfflineWork(message, say)
+        attendance.OfflineWork(appMessage(message), say)
 
 
 def addAllCommands(app):        # 모든 Command가 실행되도록 추가

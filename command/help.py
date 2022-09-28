40dbc43623bb531deacbc6be290ed359bb33c53c
Jiwonii97 <wldnjs3633@naver.com>
2022-09-28 18:54:57 +0900

    전반적인 명령어 호출 방식 변경

--- command/help.py
+++ command/help.py
@@ -1,13 +1,21 @@
 from utils.log import printCommandLogs
-from constants import COMMAND, HELP       # 상수
+from constants import COMMAND       # 상수
 
 
 def helpCommand(message, say):      # 도움
-    helpOutput = HELP.HELP_HEADLINE
+    helpOutput = COMMAND.HELP_HEADLINE
 
-    for command in COMMAND.ATTENDANCE_COMMAND_LIST:
-        helpOutput += f"{COMMAND.ATTENDANCE_COMMAND[command]} : {HELP.COMMAND_HELP_MESSAGE[command]}\n"
+    # 출력할 명령어 목록 전처리 작업 진행
+    commandList = []
+    for rgx, commands in zip(COMMAND.COMMAND_REG_LIST, COMMAND.COMMAND_LIST):
+        commandList.append([rgx, commands])
+
+    # 출력할 리스트에서 하나씩 "명령어 : 도움말" 형식으로 출력 진행
+    for commandInfo in commandList:
+        [commandReg, commands] = commandInfo
+        for command in commands:
+            helpOutput += f"{commandReg[command]} : {COMMAND.HELP_COMMAND_INFO[command]}\n"
 
     # 서버에 log를 따로 남김
-    printCommandLogs(message, COMMAND.ATTENDANCE_COMMAND[COMMAND.HELP])
-    say(text=helpOutput, channel=message['channel'])
+    printCommandLogs(message, COMMAND.HELP_COMMAND_REGEX[COMMAND.HELP])
+    say(text=helpOutput, channel=message.channel)

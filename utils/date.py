0d72363ab4d98b6b10eb5a364d41573aa7b82386
Jiwonii97 <wldnjs3633@naver.com>
2022-09-29 00:10:52 +0900

    timestamp to datetime 함수 추가

--- utils/date.py
+++ utils/date.py
@@ -4,10 +4,19 @@ from pytz import timezone
 from constants import DATE
 
 
-def getCurrentSeoulTime():
-    return datetime.now(timezone(DATE.ASIA_SEOUL)).strftime(DATE.DATE_FORMAT)
+def getCityTime(city):
+    return datetime.now(timezone(city)).strftime(DATE.DATE_FORMAT)
 
 
-def getCurrentSeoulHourMinutes():
-    times = datetime.now(timezone(DATE.ASIA_SEOUL))
+def getNowCityHourMinutes(city):
+    times = datetime.now(timezone(city))
     return times.time().hour, times.time().minute
+
+
+def ts2datetime(ts):        # Timestamp to Datetime
+    return datetime.fromtimestamp(ts, timezone(DATE.ASIA_SEOUL)).strftime(DATE.DATE_FORMAT)
+
+
+def getTs2HourMinute(ts):        # Timestamp to get Hour, Minute
+    date = datetime.fromtimestamp(ts, timezone(DATE.ASIA_SEOUL)).time()
+    return date.hour, date.minute

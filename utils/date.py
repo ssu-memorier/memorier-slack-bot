63c10acf5e53dfe06846e0386346c61bfe1cbdd9
Jiwonii97 <wldnjs3633@naver.com>
2022-09-29 20:05:47 +0900

    주석 추가

--- utils/date.py
+++ utils/date.py
@@ -4,11 +4,11 @@ from pytz import timezone
 from constants import DATE
 
 
-def getCityTime(city):
+def getCityTime(city):      # 도시 시간 구하는 함수 (추후 사용 예정)
     return datetime.now(timezone(city)).strftime(DATE.DATE_FORMAT)
 
 
-def getNowCityHourMinutes(city):
+def getNowCityHourMinutes(city):      # 도시 시분 구하는 함수 (추후 사용 예정)
     times = datetime.now(timezone(city))
     return times.time().hour, times.time().minute
 

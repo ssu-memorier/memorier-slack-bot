a58faf89292cb2d1c480e41365b2d0e5f873b34b
Jiwonii97 <wldnjs3633@naver.com>
2022-09-29 20:04:02 +0900

    함수명 수정 및 일부 호출 함수 수정

--- command/attendance.py
+++ command/attendance.py
@@ -1,11 +1,11 @@
 from constants import DATE
 from constants.COMMAND import ATTENDANCE     # 상수
 
-from utils.log import printCommandLogs, printWorkState, sayToAttendanceChannel
+from utils.log import printCommandLogs, sayWorkState, sayToAttendanceChannel
 
 
-def GoToWork(message, say):  # !출근
-    hour, minutes = message.HOUR, message.MINUTE   # 명령어를 실행한 시(HH), 분(MM)
+def goToWork(message, say):  # !출근
+    hour, minutes = message.hour, message.minute   # 명령어를 실행한 시(HH), 분(MM)
 
     # 10시 10분 넘어서 출근을 하면 지각 처리
     if hour >= DATE.LATE_TIME[DATE.HOUR] and minutes >= DATE.LATE_TIME[DATE.MIN]:
@@ -13,24 +13,18 @@ def GoToWork(message, say):  # !출근
     else:
         state = ATTENDANCE.GTW
 
-    workOutput = printWorkState(message, state)
-
+    sayWorkState(say, message, state)   # 채널 출력
     printCommandLogs(
-        message, ATTENDANCE.ATTENDANCE_COMMAND_NAME[state])      # 로그 출력
-    sayToAttendanceChannel(say, workOutput)
+        message, ATTENDANCE.COMMAND_NAME[state])      # 로그 출력
 
 
-def LeaveToWork(message, say):  # !퇴근
-    workOutput = printWorkState(message, ATTENDANCE.LTW)
-
+def leaveToWork(message, say):  # !퇴근
+    sayWorkState(say, message, ATTENDANCE.LTW)   # 채널 출력
     printCommandLogs(
-        message, ATTENDANCE.ATTENDANCE_COMMAND_NAME[ATTENDANCE.LTW])      # 로그 출력
-    sayToAttendanceChannel(say, workOutput)
+        message, ATTENDANCE.COMMAND_NAME[ATTENDANCE.LTW])      # 로그 출력
 
 
-def OfflineWork(message, say):  # !오프 (시간)
-    workOutput = printWorkState(message, ATTENDANCE.OW)
-
+def offlineWork(message, say):  # !오프 (시간)
+    sayWorkState(say, message, ATTENDANCE.OW)   # 채널 출력
     printCommandLogs(
-        message, ATTENDANCE.ATTENDANCE_COMMAND_NAME[ATTENDANCE.OW])      # 로그 출력
-    sayToAttendanceChannel(say, workOutput)
+        message, ATTENDANCE.COMMAND_NAME[ATTENDANCE.OW])      # 로그 출력

c70b957dc4f8f9c02983f4cb5134baaab785d09d
Jiwonii97 <wldnjs3633@naver.com>
2022-09-30 20:53:50 +0900

    상수명 변경

--- command/help.py
+++ command/help.py
@@ -2,7 +2,7 @@ from utils.log import printCommandLogs
 from constants.COMMAND import BASE, HELP      # 상수
 
 
-def helpCommand(message, say):      # 도움
+def helpCommand(message):      # 도움
     helpOutput = HELP.HELP_HEADLINE
 
     # 출력할 리스트에서 하나씩 "명령어 : 도움말" 형식으로 출력 진행
@@ -11,5 +11,5 @@ def helpCommand(message, say):      # 도움
             helpOutput += f"{rgx[command]} : {HELP.HELP_COMMAND_INFO[command]}\n"
 
     # 서버에 log를 따로 남김
-    printCommandLogs(message, HELP.COMMAND_REGEX[HELP.HELP])
+    printCommandLogs(message, HELP.COMMAND_IDENTIFIER[HELP.COMMAND_NAME])
     return helpOutput

04123369e71ddde2f8e377bde5b3f1bba02306e0
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 00:50:29 +0900

    에러 체크 추가 및 함수 흐름 수정

--- command/help.py
+++ command/help.py
@@ -1,13 +1,20 @@
 from utils.log import printCommandLogs
-from constants import COMMAND, HELP       # 상수
+from constants.COMMAND import BASE, HELP      # 상수
 
 
-def helpCommand(message, say):      # 도움
+def helpCommand():      # 도움
     helpOutput = HELP.HELP_HEADLINE
 
-    for command in COMMAND.ATTENDANCE_COMMAND_LIST:
-        helpOutput += f"{COMMAND.ATTENDANCE_COMMAND[command]} : {HELP.COMMAND_HELP_MESSAGE[command]}\n"
+    # 출력할 리스트에서 하나씩 "명령어 : 도움말" 형식으로 출력 진행
+    for rgx, commands in zip(BASE.COMMAND_IDENTIFIER_LIST, BASE.COMMAND_LIST):
+        for command in commands:
+            helpOutput += f"{rgx[command]} : {HELP.HELP_COMMAND_INFO[command]}\n"
 
-    # 서버에 log를 따로 남김
-    printCommandLogs(message, COMMAND.ATTENDANCE_COMMAND[COMMAND.HELP])
-    say(text=helpOutput, channel=message['channel'])
+    return helpOutput, HELP.COMMAND_IDENTIFIER[HELP.COMMAND]
+
+
+def errorCheckHelp(message):    # LeaveToWork 명령어가 제대로 들어왔는지 확인
+    messageToken = message.text.split()
+    if messageToken[0] != HELP.COMMAND_IDENTIFIER[HELP.COMMAND]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if len(messageToken) == 1 else False

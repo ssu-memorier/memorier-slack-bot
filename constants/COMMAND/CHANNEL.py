da8e53acb3861c2ef587d6411c614dac49b1ba79
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 00:50:10 +0900

    일부 상수 수정

---
+++ constants/COMMAND/CHANNEL.py
@@ -0,0 +1,5 @@
+from utils.getId import getBotChannelId
+from constants import TOKEN
+
+ATTENDANCE = '출석-테스트'
+BOT = getBotChannelId(TOKEN.TOKEN_LINK, TOKEN.READ_MODE)
---
+++ constants/COMMAND/IDENTIFIER.py
@@ -0,0 +1,10 @@
+COMMAND = "!"
+
+# Command Identifier
+HELP = "!도움"
+GOTOWORK = "!출근"
+LEAVETOWORK = "!퇴근"
+OFFLINE = "!오프"
+
+
+ATTENDANCE = [GOTOWORK, LEAVETOWORK, OFFLINE]

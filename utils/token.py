e8fd1159fda1623fe475ff683a2499d751dfc125
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 00:52:48 +0900

    테스트 토큰 데이터 수정

--- utils/token.py
+++ utils/token.py
@@ -1,13 +1,17 @@
 import json
 
 
-def getSlackToken(tokenPath, mode):
+def getSlackToken(tokenPath, mode):     # slack Token을 가져오는 함수
     # go to Keys and get dictionary
-    with open(tokenPath, mode) as f:
+    with open(tokenPath, mode, encoding='utf-8') as f:
         keys = json.load(f)
 
     # get Token
     slack_bot_token = keys['slack_token']['slack_bot_token']
     slack_app_token = keys['slack_token']['slack_app_token']
+
+    # get test Token : test 진행할 때 사용하는 TOKEN (평상시 사용 X)
+    # slack_bot_token = keys['slack_test_token']['slack_bot_token']
+    # slack_app_token = keys['slack_test_token']['slack_app_token']
 
     return slack_app_token, slack_bot_token

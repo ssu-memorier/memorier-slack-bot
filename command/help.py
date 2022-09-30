06ba07c47bd06802d2c1d581328d98e9b0a4e907
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 00:57:33 +0900

    변수명 수정

--- command/help.py
+++ command/help.py
@@ -6,9 +6,9 @@ def helpCommand():      # 도움
     helpOutput = HELP.HELP_HEADLINE
 
     # 출력할 리스트에서 하나씩 "명령어 : 도움말" 형식으로 출력 진행
-    for rgx, commands in zip(BASE.COMMAND_IDENTIFIER_LIST, BASE.COMMAND_LIST):
+    for identifier, commands in zip(BASE.COMMAND_IDENTIFIER_LIST, BASE.COMMAND_LIST):
         for command in commands:
-            helpOutput += f"{rgx[command]} : {HELP.HELP_COMMAND_INFO[command]}\n"
+            helpOutput += f"{identifier[command]} : {HELP.HELP_COMMAND_INFO[command]}\n"
 
     return helpOutput, HELP.COMMAND_IDENTIFIER[HELP.COMMAND]
 

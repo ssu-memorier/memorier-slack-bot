df1913f7901655c6e262b6428061426bd9cf3a28
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 00:49:03 +0900

    에러체크 및 명령어 실행 방식 수정

--- command/attendance.py
+++ command/attendance.py
@@ -1,38 +1,52 @@
-from constants import COMMAND, DATE     # 상수
+from constants import DATE
+from constants.COMMAND import ATTENDANCE     # 상수
 
-from utils.date import getCurrentSeoulTime
-from utils.log import printCommandLogs, printWorkState
+from utils import log, date
+
+''' 명령어 실행문 '''
 
 
-def GoToWork(message, say):  # !출근
-    dates = getCurrentSeoulTime()       # 현재시간을 가져온다
-    hour, minutes = dates.time().hour, dates.time().minute   # 명령어를 실행한 시(HH), 분(MM)
-
+def goToWork(message):  # !출근
     # 10시 10분 넘어서 출근을 하면 지각 처리
-    if hour >= DATE.LATE_TIME[DATE.HOUR] and minutes >= DATE.LATE_TIME[DATE.MIN]:
-        state = COMMAND.LATE
+    if date.isTimeInBetween(message.ts, DATE.ATTENDANCE_TIME):
+        state = ATTENDANCE.GTW
     else:
-        state = COMMAND.GTW
+        state = ATTENDANCE.LATE
 
-    workOutput = printWorkState(message, dates, state)
-
-    printCommandLogs(message, COMMAND.ATTENDANCE_COMMAND[state])      # 로그 출력
-    say(text=workOutput, channel=COMMAND.CHANNEL_ATTENDANCE)
+    outputText = log.printWorkState(message, state)   # 채널 출력문 생성
+    return outputText, ATTENDANCE.COMMAND_NAME[state]
 
 
-def LeaveToWork(message, say):  # !퇴근
-    date = getCurrentSeoulTime()       # 현재시간을 가져온다
-    workOutput = printWorkState(message, date, COMMAND.LTW)
-
-    printCommandLogs(
-        message, COMMAND.ATTENDANCE_COMMAND[COMMAND.LTW])      # 로그 출력
-    say(text=workOutput, channel=COMMAND.CHANNEL_ATTENDANCE)
+def leaveToWork(message):  # !퇴근
+    outputText = log.printWorkState(message, ATTENDANCE.LTW)   # 채널 출력문 생성
+    return outputText, ATTENDANCE.COMMAND_NAME[ATTENDANCE.LTW]
 
 
-def OfflineWork(message, say):  # !오프 (시간)
-    date = getCurrentSeoulTime()       # 현재시간을 가져온다
-    workOutput = printWorkState(message, date, COMMAND.OW)
+def offlineWork(message):  # !오프 (시간)
+    outputText = log.printWorkState(message, ATTENDANCE.OW)   # 채널 출력문 생성
+    return outputText, ATTENDANCE.COMMAND_NAME[ATTENDANCE.OW]
 
-    printCommandLogs(
-        message, COMMAND.ATTENDANCE_COMMAND[COMMAND.OW])      # 로그 출력
-    say(text=workOutput, channel=COMMAND.CHANNEL_ATTENDANCE)
+
+''' 명령어 에러 체크 '''
+
+
+def errorCheckGoToWork(message):    # GoToWork 명령어가 제대로 들어왔는지 확인
+    messageToken = message.text.split()
+    if messageToken[0] != ATTENDANCE.COMMAND_IDENTIFIER[ATTENDANCE.GTW]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if len(messageToken) == 1 else False
+
+
+def errorCheckLeaveToWork(message):    # LeaveToWork 명령어가 제대로 들어왔는지 확인
+    messageToken = message.text.split()
+    if messageToken[0] != ATTENDANCE.COMMAND_IDENTIFIER[ATTENDANCE.LTW]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if len(messageToken) == 1 else False
+
+
+def errorCheckOfflineWork(message):    # OfflineWork 명령어가 제대로 들어왔는지 확인
+    # 자리비움 예약 미구현 상태
+    messageToken = message.text.split()
+    if messageToken[0] != ATTENDANCE.COMMAND_IDENTIFIER[ATTENDANCE.OW]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if (len(messageToken) == 2) and (messageToken[1].isdigit()) else False

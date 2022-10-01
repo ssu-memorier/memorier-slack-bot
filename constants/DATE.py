76c43b5165d8d3738d05f2e191dc061474eb5723
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 21:43:28 +0900

    코어타임 및 출석 시간 추가

--- constants/DATE.py
+++ constants/DATE.py
@@ -2,11 +2,25 @@
 HOUR = 'hour'       # 시(HH)
 MIN = 'minutes'     # 분(MM)
 
+START_TIME = "startTime"    # 시작시간
+END_TIME = "endTime"        # 종료시간
+
 ASIA_SEOUL = 'Asia/Seoul'       # 서울 지역을 나타냄
 BASE_TIME = 'Etc/GMT+1'     # 서울 : 10시 == GMT+1 : 00시 (출석체크 및 주간보고서에 사용할 시간)
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'       # 출력 내용 (년-월-일 시:분:초)
 
-LATE_TIME = {
-    HOUR: 10,
-    MIN: 10
+ATTENDANCE_TIME = {
+    START_TIME: {
+        HOUR: 9, MIN: 30
+    }, END_TIME: {
+        HOUR: 10, MIN: 10
+    }
 }     # 지각 처리 시간
+
+CORE_TIME = {
+    START_TIME: {
+        HOUR: 20, MIN: 00
+    }, END_TIME: {
+        HOUR: 23, MIN: 00
+    }
+}       # 코어 타임

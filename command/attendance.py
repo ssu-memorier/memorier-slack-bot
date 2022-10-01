ca8e041258b2b5544f1bf49130891aa29c01d113
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 21:42:28 +0900

    각 커맨드 함수 흐름 변경 및 에러 체크 추가

--- command/attendance.py
+++ command/attendance.py
@@ -1,35 +1,52 @@
 from constants import DATE
 from constants.COMMAND import ATTENDANCE     # 상수
 
-from utils.log import printCommandLogs, printWorkState
+from utils import log, date
+
+''' 명령어 실행문 '''
 
 
 def goToWork(message):  # !출근
-    hour, minutes = message.hour, message.minute   # 명령어를 실행한 시(HH), 분(MM)
-
     # 10시 10분 넘어서 출근을 하면 지각 처리
-    if hour >= DATE.LATE_TIME[DATE.HOUR] and minutes >= DATE.LATE_TIME[DATE.MIN]:
-        state = ATTENDANCE.LATE
-    else:
+    if date.isTimeInBetween(message.ts, DATE.ATTENDANCE_TIME):
         state = ATTENDANCE.GTW
+    else:
+        state = ATTENDANCE.LATE
 
-    outputText = printWorkState(message, state)   # 채널 출력
-    printCommandLogs(message, ATTENDANCE.COMMAND_NAME[state])      # 로그 출력
-
-    return outputText
+    outputText = log.printWorkState(message, state)   # 채널 출력문 생성
+    return outputText, ATTENDANCE.COMMAND_NAME[state]
 
 
 def leaveToWork(message):  # !퇴근
-    outputText = printWorkState(message, ATTENDANCE.LTW)   # 채널 출력
-    printCommandLogs(
-        message, ATTENDANCE.COMMAND_NAME[ATTENDANCE.LTW])      # 로그 출력
-
-    return outputText
+    outputText = log.printWorkState(message, ATTENDANCE.LTW)   # 채널 출력문 생성
+    return outputText, ATTENDANCE.COMMAND_NAME[ATTENDANCE.LTW]
 
 
 def offlineWork(message):  # !오프 (시간)
-    outputText = printWorkState(message, ATTENDANCE.OW)   # 채널 출력
-    printCommandLogs(
-        message, ATTENDANCE.COMMAND_NAME[ATTENDANCE.OW])      # 로그 출력
+    outputText = log.printWorkState(message, ATTENDANCE.OW)   # 채널 출력문 생성
+    return outputText, ATTENDANCE.COMMAND_NAME[ATTENDANCE.OW]
 
-    return outputText
+
+''' 명령어 에러 체크 '''
+
+
+def errorCheckGoToWork(message):    # GoToWork 명령어가 제대로 들어왔는지 확인
+    messageToken = message.text.split()
+    if messageToken[0] != ATTENDANCE.COMMAND_IDENTIFIER[ATTENDANCE.GTW]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if len(messageToken) == 1 else False
+
+
+def errorCheckLeaveToWork(message):    # LeaveToWork 명령어가 제대로 들어왔는지 확인
+    messageToken = message.text.split()
+    if messageToken[0] != ATTENDANCE.COMMAND_IDENTIFIER[ATTENDANCE.LTW]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if len(messageToken) == 1 else False
+
+
+def errorCheckOfflineWork(message):    # OfflineWork 명령어가 제대로 들어왔는지 확인
+    # 자리비움 예약 미구현 상태
+    messageToken = message.text.split()
+    if messageToken[0] != ATTENDANCE.COMMAND_IDENTIFIER[ATTENDANCE.OW]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if (len(messageToken) == 2) and (messageToken[1].isdigit()) else False
--- command/help.py
+++ command/help.py
@@ -2,14 +2,19 @@ from utils.log import printCommandLogs
 from constants.COMMAND import BASE, HELP      # 상수
 
 
-def helpCommand(message):      # 도움
+def helpCommand():      # 도움
     helpOutput = HELP.HELP_HEADLINE
 
     # 출력할 리스트에서 하나씩 "명령어 : 도움말" 형식으로 출력 진행
-    for rgx, commands in zip(BASE.COMMAND_REG_LIST, BASE.COMMAND_LIST):
+    for identifier, commands in zip(BASE.COMMAND_IDENTIFIER_LIST, BASE.COMMAND_LIST):
         for command in commands:
-            helpOutput += f"{rgx[command]} : {HELP.HELP_COMMAND_INFO[command]}\n"
+            helpOutput += f"{identifier[command]} : {HELP.HELP_COMMAND_INFO[command]}\n"
 
-    # 서버에 log를 따로 남김
-    printCommandLogs(message, HELP.COMMAND_IDENTIFIER[HELP.COMMAND_NAME])
-    return helpOutput
+    return helpOutput, HELP.COMMAND_IDENTIFIER[HELP.COMMAND]
+
+
+def errorCheckHelp(message):    # LeaveToWork 명령어가 제대로 들어왔는지 확인
+    messageToken = message.text.split()
+    if messageToken[0] != HELP.COMMAND_IDENTIFIER[HELP.COMMAND]:    # 설마 명령어가 잘못되지는 않았겠지?
+        return False
+    return True if len(messageToken) == 1 else False

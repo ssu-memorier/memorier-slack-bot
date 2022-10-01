a54b8a73378a01f3b3ce2dd3191b55b2283fca4c
Jiwonii97 <wldnjs3633@naver.com>
2022-10-01 21:42:56 +0900

    일부 상수 식별자 변경

--- constants/COMMAND/ATTENDANCE.py
+++ constants/COMMAND/ATTENDANCE.py
@@ -5,13 +5,13 @@ GTW, LTW, OW, LATE = "GoToWork", "LeaveToWork", "OfflineWork", "late"
 COMMAND_NAME = {        # Command name list
     GTW: "출근",
     LTW: "퇴근",
-    OW: "자리비움(오프)",
+    OW: "오프",
     LATE: "지각"
 }
 COMMAND_LIST = [GTW, LTW, OW]  # Command list
-COMMAND_IDENTIFIER = {        # Command Keyword
+COMMAND_IDENTIFIER = {        # Command Identifier
     GTW: IDENTIFIER.COMMAND + COMMAND_NAME[GTW],       # "!출근"
     LTW: IDENTIFIER.COMMAND + COMMAND_NAME[LTW],       # "!퇴근"
-    OW: IDENTIFIER.COMMAND + COMMAND_NAME[OW],         # "!자리비움(오프)"
+    OW: IDENTIFIER.COMMAND + COMMAND_NAME[OW],         # "!오프"
     LATE: IDENTIFIER.COMMAND + COMMAND_NAME[LATE]      # "!지각"
 }

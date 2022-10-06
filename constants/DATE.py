37f772c3e614219d85393ded61200bf3063b2584
Jiwonii97 <wldnjs3633@naver.com>
2022-10-06 22:27:49 +0900

    서울시간(GMT+9) 적용으로 인한 상수 추가

--- constants/DATE.py
+++ constants/DATE.py
@@ -9,6 +9,7 @@ ASIA_SEOUL = 'Asia/Seoul'       # 서울 지역을 나타냄
 BASE_TIME = 'Etc/GMT+1'     # 서울 : 10시 == GMT+1 : 00시 (출석체크 및 주간보고서에 사용할 시간)
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'       # 출력 내용 (년-월-일 시:분:초)
 
+DIFF_TIMESTAMP_GMT9 = 3600*9        # GMT+9 와의 timestamp 차이
 WORKINGHOUR = 8         # 근무시간
 
 ATTENDANCE_TIME = {

ffc5f86abd4a18ac7fc0656a55b36119bb901481
Jiwonii97 <wldnjs3633@naver.com>
2022-10-07 00:18:37 +0900

    시간대 변경 함수 추가 및 사이 시간 계산 함수 수정

--- utils/date.py
+++ utils/date.py
@@ -30,23 +30,26 @@ def getTs2HourMinute(ts):        # Timestamp to get Hour, Minute
     return date.hour, date.minute
 
 
+def convertTs2SeoulTime(ts):        # Convert timestamp to Seoul Timestamp
+    basedateTime = datetime.fromtimestamp(float(ts))
+    seoulDateTime = basedateTime.astimezone(timezone(DATE.ASIA_SEOUL))
+    return time.mktime(seoulDateTime.timetuple())
+
+
 ''' Calculate the time between '''
 
 
 def isTimeInBetween(timestamp, baseTime):   # 해당 시간이 구하고자하는 시간 사이에 있는가
-    checkdate = datetime.fromtimestamp(
-        float(timestamp), timezone(DATE.ASIA_SEOUL)).date()  # 오늘 날짜값 추출
+    checkdate = datetime.fromtimestamp(float(timestamp)).date()  # 오늘 날짜값 추출
 
     startTime = datetime(checkdate.year, checkdate.month, checkdate.day,
                          baseTime[DATE.START_TIME][DATE.HOUR],
                          baseTime[DATE.START_TIME][DATE.MIN], 0)
-    startTimestamp = time.mktime(
-        startTime.timetuple()) - DATE.DIFF_TIMESTAMP_GMT9  # 기준 시작시간
+    startTimestamp = time.mktime(startTime.timetuple())  # 기준 시작시간
 
     endTime = datetime(checkdate.year, checkdate.month, checkdate.day,
                        baseTime[DATE.END_TIME][DATE.HOUR],
                        baseTime[DATE.END_TIME][DATE.MIN], 0)
-    endTimestamp = time.mktime(endTime.timetuple()) - \
-        DATE.DIFF_TIMESTAMP_GMT9  # 기준 종료시간
+    endTimestamp = time.mktime(endTime.timetuple())  # 기준 종료시간
 
     return True if startTimestamp <= float(timestamp) <= endTimestamp else False
